60a98da77d98d3eb540f24847a14539afa4906d7
ijul <ijuldz@gmail.com>
2024-11-11 20:17:53 +0700

    init

--- src/components/ScreenFour.vue
+++ src/components/ScreenFour.vue
@@ -29,7 +29,7 @@
                 d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1115 0z"
               />
             </svg>
-            Pencil, RT 3 RW 3, Wuryorejo, Wonogiri
+            Wonogiri
           </div>
         </div>
       </div>
